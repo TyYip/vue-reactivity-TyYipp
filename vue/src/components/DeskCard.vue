5bd24cc8f795d364e0d69b1bb66a09727343bf4f
TyYipp <144461219+TyYipp@users.noreply.github.com>
2024-02-05 10:13:45 -0500

    2/5
    goofy tutorial

---
+++ vue/src/components/DeskCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <DestCard v-for="destination in destination" :key="destination.name" :Destination="destination"/>>
+        <img :src="Destination.img" alt="">
+        <h2>{{ Destination.name }}</h2>
+        <h3>{{ Clicked }}</h3>
+        <button @clicked="increment">Click Me</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+    Destination: Object,
+});
+
+const clicked = ref(0);
+function increment() {
+    clicked++;
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
